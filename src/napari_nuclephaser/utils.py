45adad7ac670207f3bac32b45fe84da0709a0662
nikvo1 <121630412+nikvo1@users.noreply.github.com>
2026-05-30 17:09:18 +0300

    Pin versions, fix max_det (#32)
    * Pin sahi version to 0.11.36
    
    * Add exclusions to dependabot
    
    * Change max_det in model initialization to 10000
    
    * Fix yolov5 overrides max_det error

--- src/napari_nuclephaser/utils.py
+++ src/napari_nuclephaser/utils.py
@@ -17,6 +17,8 @@ def initialize_model(model_path, confidence_threshold, device):
                 confidence_threshold=confidence_threshold,
                 device=device,
             )
+            if model_type != "yolov5":
+                detection_model.model.overrides["max_det"] = 10000
             return (
                 detection_model,
                 model_type,
