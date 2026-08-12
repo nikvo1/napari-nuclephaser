0406fa892b18ce637551e82c194af683d02595d3
Nikita Voloshin <nikita.voloshin.98@gmail.com>
2026-08-12 16:20:12 +0300

    Apply in place patch to enable edge cases filtering in get_sliced_prediction function

--- src/napari_nuclephaser/__init__.py
+++ src/napari_nuclephaser/__init__.py
@@ -1,2 +1,7 @@
-__version__ = "0.0.1"
+__version__ = "0.4.0"
 # from .widget import widget_factory
+# my_project/__init__.py
+from .patches import apply_sahi_patches
+
+# Apply the SAHI patch as soon as your package is imported.
+apply_sahi_patches()
